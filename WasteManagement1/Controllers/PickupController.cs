fe7f46c76ec2576ddd8080ec4bfdb3dfb0887cd8
Jose Martinez <jose.a.m089@gmail.com>
2019-05-01 14:24:05 -0500

    Created Customer, Employee and Pickup Controller read and write layout without actions. Created Views for Customer and Employee.

---
+++ WasteManagement1/Controllers/PickupController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WasteManagement1.Controllers
+{
+    public class PickupController : Controller
+    {
+        // GET: Pickup
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Pickup/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Pickup/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pickup/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pickup/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Pickup/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pickup/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Pickup/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
