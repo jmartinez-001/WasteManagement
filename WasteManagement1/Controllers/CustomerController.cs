b5916b8c61124e43d62b508d29c9d0429c6efaee
Jose Martinez <jose.a.m089@gmail.com>
2019-05-01 21:08:57 -0500

    Added CustomerController business logic. Need to create and call an api request for the geocoding of addresses, for lat and long properties.

--- WasteManagement1/Controllers/CustomerController.cs
+++ WasteManagement1/Controllers/CustomerController.cs
@@ -1,40 +1,56 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WasteManagement1.Models;
 
 namespace WasteManagement1.Controllers
 {
     public class CustomerController : Controller
     {
+        ApplicationDbContext db;
+        public CustomerController()
+        {
+            db = new ApplicationDbContext();
+        }
+
         // GET: Customer
         public ActionResult Index()
         {
+            string id = User.Identity.GetUserId();
+            Customer customer = db.Customers.Where(c => c.UserId == id).FirstOrDefault();
             return View();
         }
 
         // GET: Customer/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details()
         {
             return View();
         }
 
         // GET: Customer/Create
-        public ActionResult Create()
+        public ActionResult Service()
         {
-            return View();
+            Pickup newPickUp = new Pickup();
+            string id = User.Identity.GetUserId();
+            Customer customer = db.Customers.Where(c => c.UserId == id).FirstOrDefault();
+            return View(customer);
         }
 
         // POST: Customer/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Service(Customer Customer, Pickup newPickup)
         {
+            string id = User.Identity.GetUserId();
+            Customer customer = db.Customers.Where(c => c.UserId == id).FirstOrDefault();
+            
             try
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
+                db.Pickups.Add(newPickup);
+                db.SaveChanges();
+                return RedirectToAction("Service");
             }
             catch
             {
@@ -43,20 +59,28 @@ namespace WasteManagement1.Controllers
         }
 
         // GET: Customer/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Profile()
         {
-            return View();
+            string id = User.Identity.GetUserId();
+            Customer customer = db.Customers.Where(c => c.UserId == id).FirstOrDefault();
+            return View(customer);
         }
 
         // POST: Customer/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Profile(Customer updatedCustomer)
         {
+            string id = User.Identity.GetUserId();
+            Customer customer = db.Customers.Where(c => c.UserId == id).FirstOrDefault();
             try
             {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
+                customer.FirstName = updatedCustomer.FirstName;
+                customer.LastName = updatedCustomer.LastName;
+                customer.Address = updatedCustomer.Address;
+                customer.City = updatedCustomer.City;
+                customer.State = updatedCustomer.State;
+                customer.ZipCode = updatedCustomer.ZipCode;
+                return RedirectToAction("Profile");
             }
             catch
             {
@@ -65,25 +89,25 @@ namespace WasteManagement1.Controllers
         }
 
         // GET: Customer/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
 
-        // POST: Customer/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+        //// POST: Customer/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
